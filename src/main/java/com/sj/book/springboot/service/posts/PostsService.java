e117d8c9503b8bb535fd770b3af361c728c298d4
loky13@daum.net <loky13@daum.net>
2022-08-28 22:14:07 +0900

    156 page
    posts 수정 및 조회 추가

--- src/main/java/com/sj/book/springboot/service/posts/PostsService.java
+++ src/main/java/com/sj/book/springboot/service/posts/PostsService.java
@@ -2,12 +2,16 @@ package com.sj.book.springboot.service.posts;
 
 import com.sj.book.springboot.domain.posts.Posts;
 import com.sj.book.springboot.domain.posts.PostsRepository;
+import com.sj.book.springboot.web.dto.PostsListResponseDto;
 import com.sj.book.springboot.web.dto.PostsResponseDto;
 import com.sj.book.springboot.web.dto.PostsSaveRequestDto;
 import com.sj.book.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -35,5 +39,14 @@ public class PostsService {
                 () -> new IllegalArgumentException("해당 게시글이 없습니다. id = " + id));
 
         return new PostsResponseDto(entity);
+    }
+
+    @Transactional(readOnly = true) //트랜잭션 범위는 유지하되, 조회 기능만 남겨두어
+    //조회 속도가 개선되기 때문에 등록,수정,삭제 기능이 전혀 없는 서비스 메소드에서 사용하는것을 추천
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc().stream()
+//                .map(PostsListResponseDto::new)
+                .map(posts -> new PostsListResponseDto(posts))
+                .collect(Collectors.toList());
     }
 }
---
+++ src/main/resources/static/js/app/index.js
@@ -0,0 +1,57 @@
+const main = {
+    init: function() {
+        let _this = this;
+        $('#btn-save').on('click', () => {
+            _this.save();
+        });
+
+        $("#btn-update").on('click', () => {
+            _this.update();
+        })
+    },
+
+    save: function() {
+        let data = {
+            title: $('#title').val(),
+            author: $('#author').val(),
+            content: $('#content').val()
+        };
+
+        $.ajax({
+            type: 'POST',
+            url: '/api/v1/posts',
+            dataType: 'json',
+            contentType: 'application/json; charset=utf-8',
+            data: JSON.stringify(data),
+        }).done((response) => {
+            alert('글이 등록되었습니다.');
+            window.location.href= "/";
+        }).fail((error) => {
+            alert(JSON.stringify(error));
+        })
+    },
+
+    update: function() {
+        const data = {
+            title: $("#title").val(),
+            content: $("#content").val(),
+        };
+
+        const id = $("#id").val();
+
+        $.ajax({
+            type: "PUT",
+            url: `/api/v1/posts/${id}`,
+            dataType: 'json',
+            contentType: 'application/json; charset=utf-8',
+            data: JSON.stringify(data)
+        }).done(() => {
+            alert('글이 수정되었습니다.');
+            window.location.href = "/";
+        }).fail((error) => {
+            alert(JSON.stringify(error));
+        })
+    }
+};
+
+main.init();
